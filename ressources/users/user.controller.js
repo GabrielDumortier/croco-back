9fa6b5a4212038852b028006a077d30b84ad2dc6
Gwenael <monokini_neko@pop-os.localdomain>
2019-10-22 13:55:22 +0200

    ajout de la route /api/users/email/:email

--- ressources/users/user.controller.js
+++ ressources/users/user.controller.js
@@ -1,11 +1,12 @@
 import {User} from './user.model';
+import {emailValidator} from '../../utils/validator';
 
 // ## TODO : modifier afin de ne pas renvoyer les PW
 
 // GET /api/users/
 export const list = async (req,res)=>{
     try{
-        const users = await User.find();
+        const users = await User.find({},{password : 0});
         if(!users) return res.status(400).end();
         res.status(200).json({users:users});
     } catch(e){
@@ -16,8 +17,22 @@ export const list = async (req,res)=>{
 
 // GET /api/users/:id
 export const listOne = async (req,res)=>{
+
     try{
-        const user = await User.findOne({_id:req.params.id});
+        const user = await User.findOne({_id:req.params.id},{password : 0});
+        if(!user) user = await User.findOne({email:req.params.email},{password : 0});
+        if(!user) return res.status(404).end();
+        res.status(200).json({users:user});
+    } catch(e){
+        console.error(e)
+        res.status(400).end()
+    }
+};
+
+// GET /api/users/email/:email
+export const listOneByEmail = async (req,res)=>{
+    try{
+        const user = await User.findOne({email:req.params.email},{password : 0});
         if(!user) return res.status(404).end();
         res.status(200).json({users:user});
     } catch(e){
